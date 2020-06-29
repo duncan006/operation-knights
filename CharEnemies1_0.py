319ebe90868a8c16441c524600cb7c1c373fc700
duncan006 <67116328+duncan006@users.noreply.github.com>
2020-06-29 02:49:19 -0400

    Update CharEnemies1_0.py

--- CharEnemies1_0.py
+++ CharEnemies1_0.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import pygame
+import characterClass
 pygame.init()
 
 fps = 30
@@ -25,43 +26,7 @@ class Enemy:
         self.speed = 10
 
 
-class Player:
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.crouchHeight = 30
-        self.standHeight = 50
-        self.height = self.standHeight
-        self.width = 20
-        self.speed = 5
-        self.score = 0
-        
-        self.jumpCount = 0
-        self.jumping = False
-        self.jumpMult = 3
-        self.jumpList = [6,6,6,6,6,4,4,4,4,2,2,2,1,1,0,-1,-1,-2,-2,-2,-4,-4,-4,-4,-6,-6,-6,-6,-6]
-        
-        self.heldLeft = False
-        self.heldRight = False
-        self.crouch = False
-        
-        
-    def special_move_handler(self):
-        if self.jumping and not self.crouch:
-            self.y -= self.jumpMult * self.jumpList[self.jumpCount]
-            self.jumpCount += 1
-            if self.jumpCount > len(self.jumpList)-1:
-                self.jumpCount = 0
-                self.jumping = False
-                
-    def movePlayer(self):
-        if self.heldLeft == True:
-            self.x -= self.speed
-        if self.heldRight == True:
-            self.x += self.speed
-
-player = Player(WINDOW_WIDTH/2,WINDOW_HEIGHT-50,(255,0,0))
+player = characterClass.Player(WINDOW_WIDTH/2,WINDOW_HEIGHT-50,(255,0,0))
 
 def dropEnemies(enemyList, spawnRate):
     if random.random() < spawnRate:
